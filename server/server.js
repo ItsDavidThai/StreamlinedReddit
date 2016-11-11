66b03358d03ee5c4482f529c6e8a879da7b7bf09
ItsDavidThai <itsdavidthai@gmail.com>
2016-11-11 13:53:21 -0800

    basic server setup with oauth post setup

---
+++ server/server.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var authService = require('./services/authService.js')
+var axios = require('axios')
+var secrets = require('../src/SECRETS.js')
+var qs = require('qs')
+
+// start express
+var app = express();
+
+// use morgan middleware for logs
+app.use(morgan('dev'));
+
+// use bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// use express.static to serve client folder
+app.use(express.static(path.join(__dirname, '../dist')));
+
+
+
+/*
+  routes can be factored out however due to size will stay in one file
+*/
+// route to get token
+app.get('/services/getAccessToken', function(req, res){
+  console.log(' got to route')
+  var code = req.query.code
+  var data = {
+    grant_type: 'authorization_code',
+    code:code,
+    redirect_uri: 'http://localhost:1337'
+  }
+  data = qs.stringify(data)
+  console.log(data)
+  axios.post('https://www.reddit.com/api/v1/access_token',data,
+    {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      auth: {username: secrets.client_id, password: secrets.client_secret}
+  })
+  .then(function (response) {
+    var data = response.data
+    res.send(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
+
+
+
+
+// route all unspecified paths to home
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// set port
+var port = process.env.PORT || 1337;
+
+// listen on port
+app.listen(port);
+
+console.log("Server is listening on port " + port);
+
